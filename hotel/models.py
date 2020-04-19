efa93e6c9033490c6bf756b15fd6df3550ba44ca
Abhijit Kumbhar <abs001>
2020-04-19 13:46:04 +0530

    Created models: Tables, Menu Category, Menus, Order

--- hotel/models.py
+++ hotel/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Tables(models.Model):
+    chairs = models.IntegerField()
+    occupied = models.BooleanField(default=False)
+
+
+class MenuCategory(models.Model):
+    category = models.TextField(max_length=100)
+
+
+class Menus(models.Model):
+    menu_name = models.TextField(max_length=200)
+    menu_description = models.TextField()
+    menu_category = models.ForeignKey(MenuCategory, on_delete=models.CASCADE)
+    menu_price = models.FloatField()
+
+
+class Order(models.Model):
+    table_number = models.ForeignKey(Tables, on_delete=models.CASCADE())
+    order_amount = models.FloatField()
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+
+
+class OrderItem(models.Model):
+    order_number = models.ForeignKey(Order, on_delete=models.CASCADE)
+    item_name = models.ForeignKey(Menus, on_delete=models.PROTECT)
+    price = models.FloatField()
+    qty = models.IntegerField()
+    total_amount = models.FloatField()
